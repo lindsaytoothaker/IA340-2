0bd9df204c5fdaab89ba75f21d1aa6ede58efb5e
doreysj <doreysj@dukes.jmu.edu>
2024-09-18 16:39:47 -0400

    Update lab4.sql

--- lab4.sql
+++ lab4.sql
@@ -1,1 +1,33 @@
+--4.1
+
+/* No, you cannot delete Virginia from the name table because it is referred to by the other tables (it is both a primary and foreign key, 1 to M relationship). 
+You need to maintain data consistency.
+
+--4.2
+
+/* No, it will not run because you do not have records with a value of fips = 80 (invalid data).
+
+--4.3
+
+/* It is fine.
+
+--4.5
+
+/* Yes, chatGBT recognizes and reads the er diagram.
+
+--4.6
+
+/* It works perfectly */
+
+SELECT n.name, i.income, i.year
+FROM income i
+INNER JOIN name n ON i.fips = n.fips
+WHERE i.year = (SELECT MAX(year) FROM income)
+ORDER BY i.income DESC
+LIMIT 1;
+
+--4.7
+
+
+
 
